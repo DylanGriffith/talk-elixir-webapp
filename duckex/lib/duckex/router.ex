61ac8854c716fe09f0d1ca87e93648eb9c37b6d0
Dylan Griffith <dyl.griffith@gmail.com>
2016-01-07 12:36:55 +1100

    add hello world route

---
+++ duckex/lib/duckex/router.ex
@@ -0,0 +1,18 @@
+defmodule Duckex.Router do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  def start_link do
+    Plug.Adapters.Cowboy.http __MODULE__, []
+  end
+
+  get "/" do
+    send_resp(conn, 200, "<h1>Hello, Duck!</h1>")
+  end
+
+  match _ do
+    send_resp(conn, 404, "oops")
+  end
+end
