bafe70c184e13244021f2e11e3bdae4a306b2487
Dylan Griffith <dyl.griffith@gmail.com>
2016-01-08 12:21:19 +1100

    add estimate page

--- duckex/lib/duckex/router.ex
+++ duckex/lib/duckex/router.ex
@@ -12,6 +12,14 @@ defmodule Duckex.Router do
     send_resp(conn, 200, Duckex.Template.index)
   end
 
+  get "/estimate" do
+    params = conn.query_string |> Plug.Conn.Query.decode
+    {number_of_people, ""} = Integer.parse(params["people"])
+    number_of_ducks = round(Float.ceil(number_of_people / 3))
+
+    send_resp(conn, 200, Duckex.Template.estimate(number_of_people, number_of_ducks))
+  end
+
   match _ do
     send_resp(conn, 404, "oops")
   end
--- duckex/lib/duckex/template.ex
+++ duckex/lib/duckex/template.ex
@@ -1,4 +1,6 @@
 defmodule Duckex.Template do
   require EEx
   EEx.function_from_file :def, :index, "./lib/templates/index.html.eex", []
+
+  EEx.function_from_file :def, :estimate, "./lib/templates/estimate.html.eex", [:number_of_people, :number_of_ducks]
 end
